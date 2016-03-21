b1dd1b4ac21389c8f8951680af6ad394b19e5a55
Einar Norðfjörð <einar@vortex.is>
2016-03-21 20:00:49 +0000

    change api to use local json files from server.js

--- api.js
+++ api.js
@@ -1,45 +1,10 @@
 import m from 'mithril';
 
-const ROOT = 'http://api.xbowling.com';
-const SCORE_TIMEOUT_MINUTES = 90;
-const VENUE_ID = 5395;
-
-function formatDate(d) {
-  const year = d.getUTCFullYear(),
-      month = d.getUTCMonth() + 1,
-      date = d.getUTCDate(),
-      hours = d.getUTCHours(),
-      minutes = d.getUTCMinutes(),
-      seconds = d.getUTCSeconds();
-
-  return `${year}/${month}/${date} ${hours}:${minutes}:${seconds}`;
-}
-
-function getLane(lane_number, request = m.prop()) {
-  let endDate = new Date();
-
-  if ((localStorage || {}).dev) {
-    endDate = new Date('2016-03-19T23:00:00');
-  }
-  const startDate = new Date(+endDate - (SCORE_TIMEOUT_MINUTES * 6e4));
-
-  if (localStorage && localStorage.dev) {
-    return m.request({
-      method: 'GET',
-      background: true,
-      config: request,
-      url: 'test.json'
-    });
-  }
+function getLane(lane_number) {
   return m.request({
     method: 'GET',
-    url: `${ROOT}/venue/${VENUE_ID}/lane/${lane_number}`,
-    data: {
-      from: formatDate(startDate),
-      to: formatDate(endDate)
-    },
-    background: true,
-    config: request
+    url: `/lanes/${lane_number}.json`,
+    background: true
   });
 }
 
